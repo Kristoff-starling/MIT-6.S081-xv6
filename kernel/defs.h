51675e7c8b3ea3aa2d2c1378fa41a4043def4e5a
Kristoff-starling <2227547142@qq.com>
2022-02-03 16:07:19 +0800

    [ADD] LAB_SYSCALL

--- kernel/defs.h
+++ kernel/defs.h
@@ -105,7 +105,7 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 #ifdef LAB_SYSCALL
-int             systeminfo(void);
+int             systeminfo(uint64);
 #endif
 
 // swtch.S

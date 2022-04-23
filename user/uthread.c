f68c904a91ec15d3df9d1f13b0acb4d934b5ff3d
Kristoff-starling <2227547142@qq.com>
2022-04-23 11:18:53 +0800

    [ADD] uthread

--- user/uthread.c
+++ user/uthread.c
@@ -10,15 +10,33 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
-  char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
-
+  char            stack[STACK_SIZE]; /* the thread's stack */
+  int             state;             /* FREE, RUNNING, RUNNABLE */
+  struct context *context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context *, struct context *);
               
 void 
 thread_init(void)
@@ -28,8 +46,11 @@ thread_init(void)
   // save thread 0's state.  thread_schedule() won't run the main thread ever
   // again, because its state is set to RUNNING, and thread_schedule() selects
   // a RUNNABLE thread.
+  for (struct thread *t = all_thread; t != all_thread + MAX_THREAD; t++)
+    t->state = FREE;
   current_thread = &all_thread[0];
   current_thread->state = RUNNING;
+  current_thread->context = (struct context *)(current_thread->stack);
 }
 
 void 
@@ -59,10 +80,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch(t->context, current_thread->context);
   } else
     next_thread = 0;
 }
@@ -76,7 +94,10 @@ thread_create(void (*func)())
     if (t->state == FREE) break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->context = (struct context *)(t->stack);
+  memset((void *)t->context, 0, sizeof(struct context));
+  t->context->ra = (uint64)func;
+  t->context->sp = (uint64)(t->stack + STACK_SIZE - 16);
 }
 
 void 
